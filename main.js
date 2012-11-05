bf36e6e472d6d40501a4e1109fd74dbc4f3e89bd
Murilo <murilo@murilo-Inspiron-1545.(none)>
2012-11-05 00:55:52 -0200

    Keep the shit on!

---
+++ main.js
@@ -0,0 +1,2 @@
+Idiots = new Meteor.Collection("idiots");
+IdiotsOnline = new Meteor.Collection("idiotsOnline");
---
+++ server/mainServer.js
@@ -0,0 +1,3 @@
+Meteor.startup(function () {
+    console.log('restart the dumb looker')
+});
